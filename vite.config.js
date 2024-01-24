51e98497f4829bd426b3fc8771312f16c6793b57
oanskyy <oana.anghelachi@gmail.com>
2024-01-24 22:56:41 +0200

    chore: fix build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	build: {
+		jsxInject: `import React from 'react'`,
+		target: "ES2022" // or 'es2021' or 'esnext'
+	}
 })
